43ff1ef87d41d9c999df9da103dcb9a90f4da6c2
huaweil <huaweil@nvidia.com>
2022-10-30 22:05:08 +0800

    add myself config

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,13 @@ set -x PATH $PATH ~/.local/bin
 set -x PATH $PATH ~/.cargo/bin
 set --universal FZF_DEFAULT_COMMAND 'fd'
 
+set PATH $PATH /usr/local/cuda/bin
+set LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/cuda/lib64 /usr/lib/x86_64-linux-gnu
+set GPGPU_COMPILER_EXPORT /home/huaweil/compiler_a/x86_64_Linux_release
+
+set P4USER huaweil
+set P4PORT p4proxy-zj:2006
+set P4CLIENT swrootlinux
+set P4ROOT /home/huaweil
+
+source /home/huaweil/anaconda3/etc/fish/conf.d/conda.fish
